9ba13b1a3edd594127f55da817f736ef4f787678
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-13 00:32:01 +0100

    upd

--- helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
+++ helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
@@ -1,4 +1,4 @@
 echo "Executing post deployment script"
-export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
-cd /var/app/current
-source /var/app/venv/*/bin/activate && python3 manage.py migrate
+#export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+#cd /var/app/current
+#source /var/app/venv/*/bin/activate && python3 manage.py migrate
