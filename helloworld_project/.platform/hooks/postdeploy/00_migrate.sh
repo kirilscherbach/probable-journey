dd3a19ef6495d14cb0bdc5576a7fbd56a9b08aa3
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-13 08:29:47 +0100

    modify bashrc

--- helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
+++ helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Executing post deployment script"
+# add rds creds to bashrc since aws doesn't do it
+while read -r line; do echo "export $line" >> ~/.bashrc; done < /opt/elasticbeanstalk/deployment/env
+# add env vars to current session
 export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+# apply migrations
 cd /var/app/current
 source /var/app/venv/*/bin/activate && python3 manage.py migrate
