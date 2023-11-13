56700b01f98d8cf6f1613bd84ee840fab05a5ba4
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-13 08:14:56 +0100

    add postdeploy script back

--- helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
+++ helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 echo "Executing post deployment script"
-#export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
-#cd /var/app/current
-#source /var/app/venv/*/bin/activate && python3 manage.py migrate
+export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+cd /var/app/current
+source /var/app/venv/*/bin/activate && python3 manage.py migrate
