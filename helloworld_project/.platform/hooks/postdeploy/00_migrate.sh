b5e2353bdf795a38311d428fa2d20e838ea8a4d8
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-18 23:23:07 +0100

    always successful createsu

--- helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
+++ helloworld_project/.platform/hooks/postdeploy/00_migrate.sh
@@ -8,4 +8,4 @@ echo "Executing post deployment script"
 cd /var/app/current
 source /var/app/venv/*/bin/activate
 python3 manage.py migrate
-python3 manage.py createsuperuser --noinput
+python3 manage.py createsuperuser --noinput || true #do not fail in case this the not the first deployment and username is taken
