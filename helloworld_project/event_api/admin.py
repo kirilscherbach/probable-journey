1ce1e7d4b200ff968c31d5673a358164dd060993
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-11-29 15:54:25 +0000

    register models

--- helloworld_project/event_api/admin.py
+++ helloworld_project/event_api/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Event, EventPlan
+
+admin.site.register(Event)
+admin.site.register(EventPlan)
