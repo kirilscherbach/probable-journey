e33c105423ccf97e91571bbdb1ae270b698db900
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2023-12-02 14:22:51 +0000

    add authentication

--- helloworld_project/event_api/views.py
+++ helloworld_project/event_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Event, SongAtEventMapping
 from .serializers import EventSerializer, SongAtEventMappingSerializer
@@ -6,6 +7,14 @@ from .serializers import EventSerializer, SongAtEventMappingSerializer
 
 class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
+
+    def get_permissions(self):
+        """
+        Override to customize permission for different actions.
+        """
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def get_queryset(self):
         queryset = Event.objects.all()
@@ -18,10 +27,19 @@ class EventList(generics.ListCreateAPIView):
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class SongAtEventMappingList(generics.ListCreateAPIView):
     serializer_class = SongAtEventMappingSerializer
+
+    def get_permissions(self):
+        """
+        Override to customize permission for different actions.
+        """
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def get_queryset(self):
         queryset = SongAtEventMapping.objects.all()
@@ -34,3 +52,4 @@ class SongAtEventMappingList(generics.ListCreateAPIView):
 class SongAtEventMappingDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SongAtEventMappingSerializer
     queryset = SongAtEventMapping.objects.all()
+    permission_classes = [IsAuthenticated]
