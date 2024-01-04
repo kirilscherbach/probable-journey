7ac85786b5ac3b9cc70db16305b951726300bf38
extern-scherbach-kiril <kiril.scherbach@gmail.com>
2024-01-05 00:18:15 +0100

    Add tests for event attendee list view

--- helloworld_project/event_api/tests/test_views.py
+++ helloworld_project/event_api/tests/test_views.py
@@ -57,3 +57,73 @@ def test_song_at_event_mapping_detail():
     url = reverse("mappings_detail", kwargs={"pk": song_at_event_mapping.pk})
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_event_attendee_list_unauthenticated():
+    client = APIClient()
+    url = reverse("event_attendees")
+    response = client.get(url)
+    assert (
+        response.status_code == 401
+    )  # Unauthenticated users should get a 403 response
+
+
+@pytest.mark.django_db
+def test_event_attendee_list_authenticated():
+    client = APIClient()
+    # Create a test user and log them in
+    user = mixer.blend(User)
+    client.force_authenticate(user=user)
+
+    # Create an event with the user as the organizer
+    event = mixer.blend(Event, event_organizer=user)
+
+    # Create an EventAttendee instance for the event
+    mixer.blend(EventAttendee, event=event)
+
+    url = reverse("event_attendees")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.data) == 1  # The user should see one event attendee
+
+
+@pytest.mark.django_db
+def test_event_attendee_list_create():
+    client = APIClient()
+    # Create a test user and log them in
+    user = mixer.blend(User)
+    client.force_authenticate(user=user)
+
+    # Create an event with the user as the organizer
+    event = mixer.blend(Event, event_organizer=user)
+
+    # Create another user to be the attendee
+    attendee = mixer.blend(User)
+
+    url = reverse("event_attendees")
+    data = {"event": event.pk, "attendee": attendee.pk}
+    response = client.post(url, data)
+    assert response.status_code == 201  # The creation should be successful
+    assert EventAttendee.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_event_attendee_list_create_not_organizer():
+    client = APIClient()
+    # Create a test user and log them in
+    user = mixer.blend(User)
+    client.force_authenticate(user=user)
+
+    # Create an event with a different user as the organizer
+    event = mixer.blend(Event, event_organizer=mixer.blend(User))
+
+    # Create another user to be the attendee
+    attendee = mixer.blend(User)
+
+    url = reverse("event_attendees")
+    data = {"event": event.pk, "attendee": attendee.pk}
+    response = client.post(url, data)
+    assert (
+        response.status_code == 403
+    )  # The user should not be allowed to create the event attendee
